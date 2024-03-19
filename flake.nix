bde1929312a8d5e3bed881bda15126d4a5c08e6d
7FM <41307817+7FM@users.noreply.github.com>
2024-03-19 19:21:45 +0100

    nix setup, make compile with newer platformio versions

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "env flake";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          platformio
+        ];
+      };
+    }
+  );
+}
